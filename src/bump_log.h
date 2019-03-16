98cc6dd5f0229fadc22f06a93a76239103f85bfa
Chun Meng <mengchun0120@gmail.com>
2019-03-15 21:13:09 -0400

    Added log & shaders

---
+++ src/bump_log.h
@@ -0,0 +1,80 @@
+#ifndef INCLUDE_LOG
+#define INCLUDE_LOG
+
+#include <stdio.h>
+
+namespace bump {
+
+class Logger {
+public:
+    enum LogLevel {
+        LEVEL_DEBUG,
+        LEVEL_INFO,
+        LEVEL_WARN,
+        LEVEL_ERROR
+    };
+
+    static Logger& getSingleton();
+
+    static bool initSingleton(const char* logFile, LogLevel minLevel);
+
+    static const char* levelString(LogLevel level);
+
+    ~Logger();
+
+    void write(const char *format, ...);
+
+    void logTime();
+
+    LogLevel minLevel() const
+    {
+        return m_minLevel;
+    }
+
+    bool valid() const
+    {
+        return m_fp != nullptr;
+    }
+
+    void flush();
+
+    void close();
+
+private:
+    Logger(const char* logFile, LogLevel minLevel);
+
+    FILE *m_fp;
+    LogLevel m_minLevel;
+};
+
+} // end of namespace bump
+
+#ifdef ENABLE_LOG
+#define LOG_WRITE(level, ...) \
+    { \
+        bump::Logger& logger = bump::Logger::getSingleton(); \
+        if(logger.valid() && level >= logger.minLevel()) { \
+            logger.logTime(); \
+            logger.write(" %s:%d %s ", __FILE__, __LINE__, bump::Logger::levelString(level)); \
+            logger.write(__VA_ARGS__); \
+            logger.write("\n"); \
+            logger.flush(); \
+        } \
+    }
+#else
+#define LOG_WRITE(level, ...)
+#endif
+
+#define LOG_DEBUG(...) \
+    LOG_WRITE(bump::Logger::LEVEL_DEBUG, __VA_ARGS__)
+
+#define LOG_INFO(...) \
+    LOG_WRITE(bump::Logger::LEVEL_INFO, __VA_ARGS__)
+
+#define LOG_WARN(...) \
+    LOG_WRITE(bump::Logger::LEVEL_WARN, __VA_ARGS__)
+
+#define LOG_ERROR(...) \
+    LOG_WRITE(bump::Logger::LEVEL_ERROR, __VA_ARGS__)
+
+#endif
--- src/bump_main.cpp
+++ src/bump_main.cpp
@@ -1,4 +1,21 @@
+#include <iostream>
+#include "bump_log.h"
+
 int main(int argc, char *argv[])
 {
+    if(argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " configFile logFile" << std::endl;
+        return 1;
+    }
+
+    #ifdef ENABLE_LOG
+    if(!bump::Logger::initSingleton(argv[2], bump::Logger::LEVEL_DEBUG)) {
+        std::cerr << "Failed to setup log" << std::endl;
+        return 1;
+    }
+    #endif
+
+    LOG_INFO("Program initialized");
+
     return 0;
 }
